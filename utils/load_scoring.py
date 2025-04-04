43d178377f8a9e355c6d07197272798925d0a4aa
Neokgwadi <156335066+Neokgwadi@users.noreply.github.com>
2025-04-04 19:17:21 +0200

    Update load_scoring.py

--- utils/load_scoring.py
+++ utils/load_scoring.py
@@ -1,1 +1,14 @@
-
+def calculate_lead_score(profile):
+    score = 0
+    
+    # Score based on connections
+    if profile.get('connections', 0) > 500:
+        score += 3
+    elif profile.get('connections', 0) > 100:
+        score += 1
+        
+    # Score based on activity
+    if profile.get('last_post_days', 999) < 7:
+        score += 2
+        
+    return score
